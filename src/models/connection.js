1721486ca15924c5e4df41c0f09d194ceeb3af55
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-04 11:54:07 -0300

    initial commit

---
+++ src/models/connection.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Plant_Catalog';
+
+const DB_NAME = 'Plant_Catalog';
+
+let db = null;
+
+const getConnection = () => (db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((conn) => {
+  db = conn.db(DB_NAME);
+  return db;
+  }));
+
+module.exports = {
+getConnection,
+};
