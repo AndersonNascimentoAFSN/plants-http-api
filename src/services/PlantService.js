ca1de544d646de47dd564b4c5d8679ee417aa905
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-04 19:53:30 -0300

    feat: function create for service plantService

---
+++ src/services/PlantService.js
@@ -0,0 +1,16 @@
+const PlantModel = require('../models/PlantModel');
+const PlantSchema = require('../schemas/PlantSchema');
+
+const create = async ({ breed, needsSun, origin, size, specialCare = '' }) => {
+  const { error } = PlantSchema.plantCreateValidate({ breed, needsSun, origin, size });
+
+  if (error) return { code: 400, message: error.details[0].message };
+
+  const newPlant = await PlantModel.create({ breed, needsSun, origin, size, specialCare });
+
+  return newPlant;
+};
+
+module.exports = {
+  create,
+};
