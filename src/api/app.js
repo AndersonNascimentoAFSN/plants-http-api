cb747ab16ca4a98e82d9f274cd63f96aed35cb1e
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-05 08:30:06 -0300

    feat: cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,10 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+// app.use((_req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Method', 'GET, PUT, POST, DELETE');
+//   res.header('Access-Control-Allow-Headers', '\'X-PINGOTHER\' \'Content-Type\', \'Authorization');
+
+//   app.use(cors());
+
+//   next();
+// });
+
+app.use(cors());
 
 const router = require('../routes');
 
