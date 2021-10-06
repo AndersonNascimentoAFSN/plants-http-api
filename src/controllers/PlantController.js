5ae89df0ffc260dd80984558b6288b91fd895e72
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-06 13:11:10 -0300

    feat: add express-rescue

--- src/controllers/PlantController.js
+++ src/controllers/PlantController.js
@@ -1,6 +1,21 @@
+const rescue = require('express-rescue');
 const PlantService = require('../services/PlantService');
 
-const create = async (req, res) => {
+// const create = async (req, res, next) => {
+//   try {
+//     const { breed, needsSun, origin, size, specialCare } = req.body;
+  
+//     const newPlant = await PlantService.create({ breed, needsSun, origin, size, specialCare });
+  
+//     if (newPlant.message) return res.status(newPlant.code).json({ message: newPlant.message });
+
+//     res.status(201).json(newPlant);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+const create = rescue(async (req, res) => {
   const { breed, needsSun, origin, size, specialCare } = req.body;
 
   const newPlant = await PlantService.create({ breed, needsSun, origin, size, specialCare });
@@ -8,31 +23,31 @@ const create = async (req, res) => {
   if (newPlant.message) return res.status(newPlant.code).json({ message: newPlant.message });
 
   res.status(201).json(newPlant);
-};
+});
 
-const findAll = async (_req, res) => {
+const findAll = rescue(async (_req, res) => {
   const plants = await PlantService.findAll();
 
   res.status(200).json(plants);
-};
+});
 
-const findById = async (req, res) => {
+const findById = rescue(async (req, res) => {
   const { id } = req.params;
 
   const plant = await PlantService.findById(id);
 
   res.status(200).json(plant);
-};
+});
 
-const remove = async (req, res) => {
+const remove = rescue(async (req, res) => {
   const { id } = req.params;
 
   await PlantService.remove(id);
 
   res.status(204).end();
-};
+});
 
-const update = async (req, res) => {
+const update = rescue(async (req, res) => {
   const { id } = req.params;
   const { breed, size, needsSun, origin, specialCare } = req.body;
 
@@ -40,7 +55,7 @@ const update = async (req, res) => {
     .update(id, { breed, size, needsSun, origin, specialCare });
 
   res.status(200).json(plantUpdated);
-};
+});
 
 module.exports = {
   create,
