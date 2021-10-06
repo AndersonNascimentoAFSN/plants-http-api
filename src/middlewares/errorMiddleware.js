70e46059b172f1dfd05c6948badb6806b3e260b8
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-06 13:09:27 -0300

    refactor: middleware error

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,4 +1,8 @@
 const errorMiddleware = (error, _req, res, _next) => {
+  if (error.code && error.status) {
+    return res.status(error.status).json({ message: error.message, code: error.code });
+  }
+  
   res.status(error.status || 500);
   return res.json({
     error: {
